39827531c615728a519bdfa53aab498b844727ca
Alex <467146659@qq.com>
2017-12-16 01:16:00 +0800

    🚂add itinerary service , syuio_protocol.json

---
+++ src/itinerary/protocolService.js
@@ -0,0 +1,8 @@
+function proto1Service(proto) {
+    console.log("---")
+    console.log(proto.mType())
+    console.log(proto.uint8())
+    console.log(proto.uint16())
+    console.log(proto.str())
+    // body...
+}
--- src/syuio.js
+++ src/syuio.js
@@ -4,6 +4,15 @@ var dataLen = 0;
 var currentLen = 0;
 var receiveBuff;
 var packLen = 512;
+var protoJsonPath = "./syuio_protocol.json";
+var auto_reflex_service = true;
+
+function initSyuio(path, autoReflex) {
+    // body...
+    protoJsonPath = path;
+    auto_reflex_service = autoReflex;
+}
+
 function receive(eventdata) {
     var dataView = new DataView(eventdata);
     for (var i = 0; i < dataView.byteLength; i++) {
@@ -25,7 +34,6 @@ function receive(eventdata) {
             currentLen = 0
         }
     }
-
 }
 
 function receiveData(receiveBuff) {
@@ -33,8 +41,43 @@ function receiveData(receiveBuff) {
         return false;
     }
     var tempProtocol = new UnpackProtocol(receiveBuff, receiveBuff.byteLength);
-    var tempDataLen = tempProtocol.ReadUint16();
-    //this to unpack and reflex service
+    tempProtocol.ReadUint16();
     reflex(tempProtocol);
+
+    function reflex(tempProtocol) {
+        var mType = tempProtocol.ReadUint16();
+        $.get('syuio_protocol.json').done(function(data) {
+            for (var i = 0; i < data.length; i++) {
+                if (mType == data[i].type) {
+                    var fun = eval(data[i].funName);
+                    if (auto_reflex_service) {
+                        var service = eval(data[i].serviceName);
+                        new service(new fun(tempProtocol));
+                    } else {
+                        return new fun(tempProtocol);
+                    }
+                }
+            }
+        });
+    }
 };
 
+function proto1(tempBuff) {
+    var mType = 1;
+    var str = tempBuff.ReadString()
+    var uint8 = tempBuff.ReadUint8()
+    var uint16 = tempBuff.ReadUint16()
+    this.mType = function() {
+        return mType;
+    }
+    this.str = function() {
+        return str;
+    }
+    this.uint8 = function() {
+        return uint8;
+    }
+    this.uint16 = function() {
+        return uint16;
+    }
+    // body...
+}
